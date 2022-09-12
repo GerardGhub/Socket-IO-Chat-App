1809b4fc944738c358e833d9d133ab4b58b0ea36
GerardGhub <singian28@gmail.com>
2022-09-12 22:50:26 +0800

    add gitignore

--- httpserver.js
+++ httpserver.js
@@ -1,6 +1,13 @@
+//express JS
 var express = require("express");
 var app = express();
 app.listen(7070, startup);
 function startup() {
     console.log("Server started at port 7070");
 }
+
+//Socket IO
+var http = require("http");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
