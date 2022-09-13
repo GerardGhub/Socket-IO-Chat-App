56367e11394b5f02203b0b2a6280c7008f1a2ed7
GerardGhub <singian28@gmail.com>
2022-09-13 21:17:28 +0800

    Second Data

--- httpserver.js
+++ httpserver.js
@@ -5,9 +5,15 @@ app.listen(7070, startup);
 function startup() {
     console.log("Server started at port 7070");
 }
+app.use(express.static(__dirname));
 
 //Socket IO
 var http = require("http");
+var server = http.createServer();
+server.listen(9090);
+var socketio = require("socket.io");
+var io = socketio(server);
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
